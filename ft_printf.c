29ccea71b96090244bbfcd8ebd1c8987b862d33c
Emrullah <emrullah@student.42.fr>
2026-02-25 14:45:07 +0300

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -3,45 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etasci <etasci@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*   By: emrullah <emrullah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 20:10:34 by etasci            #+#    #+#             */
-/*   Updated: 2026/02/18 22:01:28 by etasci           ###   ########.fr       */
+/*   Updated: 2026/02/24 17:11:20 by emrullah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stdarg.h>
+#include<unistd.h>
 int	ft_printf(const char *str, ...)
 {
-	va_list	atr;
+	va_list	args;
 	int	i;
+	i = 0;
 	int count;
-	va_start (atr,str);
+	count = 0;
+	va_start (args,str);
 
 	while(str[i])
 	{
-		 if(str[i] == %)
+		 if(str[i] == '%' && str[i + 1])
 		 {
 			i++;
 			if(str[i] == 'c')
-			count += ft_print_char(va_args );
-
-
-		 }
+			count += ft_print_char(args);
+		}
+		else 
+		{
+			count += write(1,&str[i],1);
+		}
+		i++;
 	}
-
-
-
-
-
-
-
-
-
-
-
 	va_end(args);
+	return(count);
+}
+#include<stdio.h>
+int main()
+{
+int ret1 = ft_printf("Hello %c\n", 'A');
+int ret2 = printf("Hello %c\n", 'A');
 
-
-
-
+printf("ft_printf return: %d\n", ret1);
+printf("printf return: %d\n", ret2);
 }

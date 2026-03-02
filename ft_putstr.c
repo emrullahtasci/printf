7a8946376ec7bc7edccf5911469db282067fb3c2
Emrullah <emrullah@student.42.fr>
2026-03-02 15:53:14 +0300

    printf

---
+++ ft_putstr.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emrullah <emrullah@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/28 15:24:28 by emrullah          #+#    #+#             */
+/*   Updated: 2026/03/01 15:10:38 by emrullah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putstr(char *str)
+{
+	int	i;
+	int	count;
+	count = 0;
+	i = 0;
+	
+	if(!str)
+	{
+		str = "(null)";
+	}
+	while(str[i])
+	{
+		count = count + write(1,&str,1);
+		i++;
+
+	}
+	return(count);
+	
+}
+#include<stdio.h>
+int main(void)
+{
+    int ft_ret;
+    int real_ret;
+
+    // --- TEST 1: String ---
+    ft_ret = ft_printf("Test string: %s\n", "Emrullah");
+    real_ret = printf("Test string: %s\n", "Emrullah");
+
+    printf("ft_printf return: %d\n", ft_ret);
+    printf("printf return: %d\n", real_ret);
+    printf("---------------------------\n");
+
+    // --- TEST 2: Char ---
+    ft_ret = ft_printf("Test char: %c\n", 'A');
+    real_ret = printf("Test char: %c\n", 'A');
+
+    printf("ft_printf return: %d\n", ft_ret);
+    printf("printf return: %d\n", real_ret);
+    printf("---------------------------\n");
+
+    // --- TEST 3: Integer ---
+    ft_ret = ft_printf("Test int: %d\n", 42);
+    real_ret = printf("Test int: %d\n", 42);
+
+    printf("ft_printf return: %d\n", ft_ret);
+    printf("printf return: %d\n", real_ret);
+    printf("---------------------------\n");
+
+    return (0);
+}
+
+
